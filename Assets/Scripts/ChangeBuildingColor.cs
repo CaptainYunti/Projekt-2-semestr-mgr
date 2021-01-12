f1818e6560e53bcd5456d7ee79609b0a8eec2fe3
Yunti <n.pawcio@gmail.com>
2021-01-12 08:08:32 +0100

    Changing buildings color
    Each building is set Red with some probability. Rest of them are Blue. If player touch Red building, it will change color to Green.

---
+++ Assets/Scripts/ChangeBuildingColor.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeBuildingColor : MonoBehaviour
+{
+    [SerializeField]
+    int probabilityRed;
+
+    public Material[] material;
+    Renderer rend;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        int rand = Random.Range(0, probabilityRed);
+
+        rend = GetComponent<Renderer>();
+        rend.enabled = true;
+        
+        if(rand != 0)
+        {
+            rend.sharedMaterial = material[0];
+            
+        }
+        else
+        {
+            rend.sharedMaterial = material[1];
+        }
+
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (rend.sharedMaterial == material[1])
+        {
+            rend.sharedMaterial = material[2];
+        }
+    }
+
+}
